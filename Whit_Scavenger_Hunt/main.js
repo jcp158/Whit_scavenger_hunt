0064ff46b2ae6d64760437180f989d4df03e2cae
jcp158 <jcp.158.8705@gmail.com>
2025-02-05 21:25:43 -0600

    Delete Whit_Scavenger_Hunt directory

--- Whit_Scavenger_Hunt/main.js
+++
@@ -1,9 +0,0 @@
-var aTags = document.querySelectorAll('span[data-href]');
-
-for(var i = 0; i < aTags.length; i++){
-	var aTag = aTags[i];
-	aTag.addEventListener('click', function(e){
-		var ele = e.target;
-		window.location.replace(ele.getAttribute('data-href'));
-	});
-}
